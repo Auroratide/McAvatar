6c3ebcbe86c85be946e72ee0002d1aee20275e16
Timothy Foster <tf.auroratide@gmail.com>
2020-08-29 13:25:43 -0500

    Apply sound and properties to all relevant blocks

--- src/main/kotlin/mcavatar/material/Sounds.kt
+++ src/main/kotlin/mcavatar/material/Sounds.kt
@@ -12,17 +12,112 @@ data class BlockSounds(
     val broken: Sound?
 )
 
+private val grassSounds = BlockSounds(broken = Sound.BLOCK_GRASS_BREAK)
+private val gravelSounds = BlockSounds(broken = Sound.BLOCK_GRAVEL_BREAK)
+private val sandSounds = BlockSounds(broken = Sound.BLOCK_SAND_BREAK)
+private val stoneSounds = BlockSounds(broken = Sound.BLOCK_STONE_BREAK)
+
+private val sounds: Map<Material, BlockSounds> = mapOf(
+    Material.DIRT to gravelSounds,
+    Material.GRASS_BLOCK to grassSounds,
+    Material.COARSE_DIRT to gravelSounds,
+    Material.GRAVEL to gravelSounds,
+
+    Material.STONE to stoneSounds,
+    Material.STONE_SLAB to stoneSounds,
+    Material.STONE_STAIRS to stoneSounds,
+    Material.SMOOTH_STONE to stoneSounds,
+    Material.SMOOTH_STONE_SLAB to stoneSounds,
+    Material.STONE_BRICKS to stoneSounds,
+    Material.STONE_BRICK_SLAB to stoneSounds,
+    Material.STONE_BRICK_STAIRS to stoneSounds,
+    Material.STONE_BRICK_WALL to stoneSounds,
+    Material.CHISELED_STONE_BRICKS to stoneSounds,
+    Material.CRACKED_STONE_BRICKS to stoneSounds,
+    Material.MOSSY_STONE_BRICKS to stoneSounds,
+    Material.MOSSY_STONE_BRICK_SLAB to stoneSounds,
+    Material.MOSSY_STONE_BRICK_STAIRS to stoneSounds,
+    Material.MOSSY_STONE_BRICK_WALL to stoneSounds,
+
+    Material.COBBLESTONE to stoneSounds,
+    Material.COBBLESTONE_SLAB to stoneSounds,
+    Material.COBBLESTONE_STAIRS to stoneSounds,
+    Material.COBBLESTONE_WALL to stoneSounds,
+    Material.MOSSY_COBBLESTONE to stoneSounds,
+    Material.MOSSY_COBBLESTONE_SLAB to stoneSounds,
+    Material.MOSSY_COBBLESTONE_STAIRS to stoneSounds,
+    Material.MOSSY_COBBLESTONE_WALL to stoneSounds,
+
+    Material.BLACKSTONE to stoneSounds,
+    Material.BLACKSTONE_SLAB to stoneSounds,
+    Material.BLACKSTONE_STAIRS to stoneSounds,
+    Material.BLACKSTONE_WALL to stoneSounds,
+    Material.POLISHED_BLACKSTONE to stoneSounds,
+    Material.POLISHED_BLACKSTONE_BRICKS to stoneSounds,
+    Material.POLISHED_BLACKSTONE_BRICK_SLAB to stoneSounds,
+    Material.POLISHED_BLACKSTONE_BRICK_STAIRS to stoneSounds,
+    Material.POLISHED_BLACKSTONE_BRICK_WALL to stoneSounds,
+    Material.POLISHED_BLACKSTONE_SLAB to stoneSounds,
+    Material.POLISHED_BLACKSTONE_STAIRS to stoneSounds,
+    Material.POLISHED_BLACKSTONE_WALL to stoneSounds,
+    Material.CRACKED_POLISHED_BLACKSTONE_BRICKS to stoneSounds,
+
+    Material.ANDESITE to stoneSounds,
+    Material.ANDESITE_SLAB to stoneSounds,
+    Material.ANDESITE_STAIRS to stoneSounds,
+    Material.ANDESITE_WALL to stoneSounds,
+    Material.POLISHED_ANDESITE to stoneSounds,
+    Material.POLISHED_ANDESITE_SLAB to stoneSounds,
+    Material.POLISHED_ANDESITE_STAIRS to stoneSounds,
+
+    Material.GRANITE to stoneSounds,
+    Material.GRANITE_SLAB to stoneSounds,
+    Material.GRANITE_STAIRS to stoneSounds,
+    Material.GRANITE_WALL to stoneSounds,
+    Material.POLISHED_GRANITE to stoneSounds,
+    Material.POLISHED_GRANITE_SLAB to stoneSounds,
+    Material.POLISHED_GRANITE_STAIRS to stoneSounds,
+
+    Material.DIORITE to stoneSounds,
+    Material.DIORITE_SLAB to stoneSounds,
+    Material.DIORITE_STAIRS to stoneSounds,
+    Material.DIORITE_WALL to stoneSounds,
+    Material.POLISHED_DIORITE to stoneSounds,
+    Material.POLISHED_DIORITE_SLAB to stoneSounds,
+    Material.POLISHED_DIORITE_STAIRS to stoneSounds,
+
+    Material.SAND to sandSounds,
+    Material.SANDSTONE to stoneSounds,
+    Material.SANDSTONE_SLAB to stoneSounds,
+    Material.SANDSTONE_STAIRS to stoneSounds,
+    Material.SANDSTONE_WALL to stoneSounds,
+    Material.CHISELED_SANDSTONE to stoneSounds,
+    Material.CUT_SANDSTONE to stoneSounds,
+    Material.CUT_SANDSTONE_SLAB to stoneSounds,
+    Material.SMOOTH_SANDSTONE to stoneSounds,
+    Material.SMOOTH_SANDSTONE_SLAB to stoneSounds,
+    Material.SMOOTH_SANDSTONE_STAIRS to stoneSounds,
+    Material.RED_SAND to sandSounds,
+    Material.RED_SANDSTONE to stoneSounds,
+    Material.RED_SANDSTONE_SLAB to stoneSounds,
+    Material.RED_SANDSTONE_STAIRS to stoneSounds,
+    Material.RED_SANDSTONE_WALL to stoneSounds,
+    Material.CHISELED_RED_SANDSTONE to stoneSounds,
+    Material.CUT_RED_SANDSTONE to stoneSounds,
+    Material.CUT_RED_SANDSTONE_SLAB to stoneSounds,
+    Material.SMOOTH_RED_SANDSTONE to stoneSounds,
+    Material.SMOOTH_RED_SANDSTONE_SLAB to stoneSounds,
+    Material.SMOOTH_RED_SANDSTONE_STAIRS to stoneSounds,
+)
+
 fun Block.playSound(getSound: BlockSounds.() -> Sound?) {
-    val sound = sounds.getSound()
+    val sound = sounds().getSound()
 
     if (sound != null) {
-        world.playSound(location, sound, 1.0f, 1.0f)
+        world.playSound(location, sound, 1.0f, 0.75f)
     } else {
         logger().warning("Attempted sound for $type, but no sounds were found. Actual block: $this")
     }
 }
 
-val Block.sounds get() = when(type) {
-    Material.STONE -> BlockSounds(broken = Sound.BLOCK_STONE_BREAK)
-    else -> BlockSounds(broken = null)
-}
+fun Block.sounds() = sounds[type] ?: BlockSounds(broken = null)
