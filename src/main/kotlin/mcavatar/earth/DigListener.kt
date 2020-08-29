beb8ba1a491539f3d4a6d1ce4f542f256822a28c
Timothy Foster <tf.auroratide@gmail.com>
2020-08-29 12:16:19 -0500

    Time to break block is dynamic depending on hardness

--- src/main/kotlin/mcavatar/earth/DigListener.kt
+++ src/main/kotlin/mcavatar/earth/DigListener.kt
@@ -3,23 +3,25 @@ package mcavatar.earth
 import mcavatar.*
 import mcavatar.material.*
 import mcavatar.minecraft.*
-import mcavatar.scheduler.Scheduler
-import mcavatar.scheduler.Task
-import mcavatar.scheduler.ticks
+import mcavatar.scheduler.*
+import org.bukkit.Bukkit
+import org.bukkit.block.Block
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockDamageEvent
+import java.lang.Float.max
+import java.lang.Float.min
 
 class DigListener(private val scheduler: Scheduler) : Listener {
     private val breakTasks = mutableMapOf<Player, Task>()
 
     @EventHandler fun startDig(e: BlockDamageEvent) {
         if (e.block.properties().has<earthy>() && !e.itemInHand.properties().has<tool>()) {
-            breakTasks[e.player] = scheduler.runAfter(10.ticks) {
+            breakTasks[e.player] = scheduler.runAfter(e.block.timeToBreak) {
                 e.block.playSound { broken }
                 e.block.breakNaturally()
-            } + scheduler.onEachTickFor(10.ticks) {
+            } + scheduler.onEachTickFor(e.block.timeToBreak) {
                 PacketSender().send(e.player, Packet.BlockBreakAnimation(e.player, e.block, Breakage(it)))
             }
         }
@@ -30,4 +32,6 @@ class DigListener(private val scheduler: Scheduler) : Listener {
             breakTasks[player]?.cancel()
         }
     }
+
+    private val Block.timeToBreak get() = 4.ticks * max(type.hardness, 1.0f)
 }
--- src/main/kotlin/mcavatar/scheduler/Ticks.kt
+++ src/main/kotlin/mcavatar/scheduler/Ticks.kt
@@ -1,8 +1,12 @@
 package mcavatar.scheduler
 
 import java.time.Duration
+import kotlin.math.roundToLong
 
 const val MILLIS_IN_TICK = 50L
 val Int.ticks: Duration get() = Duration.ofMillis(this * MILLIS_IN_TICK)
 val Long.ticks: Duration get() = Duration.ofMillis(this * MILLIS_IN_TICK)
 fun Duration.toTicks() = toMillis() / MILLIS_IN_TICK
+
+operator fun Duration.times(multiplicand: Float): Duration =
+    Duration.ofNanos((toNanos().toDouble() * multiplicand).roundToLong())
