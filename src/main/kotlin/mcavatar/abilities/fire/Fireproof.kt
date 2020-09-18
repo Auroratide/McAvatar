fe0ec4f19b5efd4c45c5118499e05f55e99d8a8d
Timothy Foster <tf.auroratide@gmail.com>
2020-09-18 18:31:01 -0500

    Refactor fireproof to use ability interface

--- src/main/kotlin/mcavatar/abilities/fire/Fireproof.kt
+++ src/main/kotlin/mcavatar/abilities/fire/Fireproof.kt
@@ -1,13 +1,24 @@
 package mcavatar.abilities.fire
 
+import mcavatar.abilities.Ability
+import mcavatar.permissions.Bending
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.EntityDamageEvent
 
-class Fireproof {
+class Fireproof(player: Player, private val event: EntityDamageEvent) : Ability(player, Bending.Fire) {
+    override fun preconditions() = with(event) {
+        trigger { cause == EntityDamageEvent.DamageCause.FIRE_TICK }
+    }
+
+    override fun action() = with(event) {
+        isCancelled = true
+    }
+
     class Listener : org.bukkit.event.Listener {
         @EventHandler fun onDamage(e: EntityDamageEvent) {
-            if (e.cause == EntityDamageEvent.DamageCause.FIRE_TICK)
-                e.isCancelled = true
+            if (e.entity is Player)
+                Fireproof(e.entity as Player, e).execute()
         }
     }
 }
