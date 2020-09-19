fd8f5f3be63d99e758cebae6239a0441d7c58dd0
Timothy Foster <tf.auroratide@gmail.com>
2020-09-18 20:04:38 -0500

    Apply strength when on fire

--- src/main/kotlin/mcavatar/abilities/fire/Fireproof.kt
+++ src/main/kotlin/mcavatar/abilities/fire/Fireproof.kt
@@ -2,17 +2,27 @@ package mcavatar.abilities.fire
 
 import mcavatar.abilities.Ability
 import mcavatar.permissions.Bending
+import mcavatar.scheduler.toTicks
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionEffectType
+import java.time.Duration
 
 class Fireproof(player: Player, private val event: EntityDamageEvent) : Ability(player, Bending.Fire) {
     override fun preconditions() = with(event) {
-        trigger { cause == EntityDamageEvent.DamageCause.FIRE_TICK }
+        trigger {
+            cause == EntityDamageEvent.DamageCause.FIRE_TICK ||
+            cause == EntityDamageEvent.DamageCause.FIRE ||
+            cause == EntityDamageEvent.DamageCause.LAVA
+        }
     }
 
     override fun action() = with(event) {
-        isCancelled = true
+        isCancelled = cause == EntityDamageEvent.DamageCause.FIRE_TICK
+
+        player.strengthen()
     }
 
     class Listener : org.bukkit.event.Listener {
@@ -20,5 +30,13 @@ class Fireproof(player: Player, private val event: EntityDamageEvent) : Ability(
             if (e.entity is Player)
                 Fireproof(e.entity as Player, e).execute()
         }
+    }
+
+    private fun Player.strengthen() {
+        addPotionEffect(PotionEffect(
+            PotionEffectType.INCREASE_DAMAGE,
+            Duration.ofMillis(1250).toTicks().toInt(),
+            0)
+        )
     }
 }
