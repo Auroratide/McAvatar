a30b2510ffcf8d009f4426b8bb63a94e260e5004
Timothy Foster <tf.auroratide@gmail.com>
2020-09-19 13:42:46 -0500

    Light blocks on fire in front of player

--- src/main/kotlin/mcavatar/abilities/fire/Firesweep.kt
+++ src/main/kotlin/mcavatar/abilities/fire/Firesweep.kt
@@ -2,20 +2,31 @@ package mcavatar.abilities.fire
 
 import mcavatar.PacketSender
 import mcavatar.abilities.Ability
+import mcavatar.bukkit.block.perpendicular
 import mcavatar.minecraft.Animation
 import mcavatar.minecraft.ClientAnimation
 import mcavatar.minecraft.Packet
 import mcavatar.permissions.Bending
 import mcavatar.scheduler.toTicks
 import org.bukkit.Material
+import org.bukkit.block.Block
+import org.bukkit.block.BlockFace
 import org.bukkit.event.EventHandler
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.EquipmentSlot
 import java.time.Duration
+import kotlin.math.abs
+import kotlin.math.floor
+import kotlin.math.roundToInt
+import kotlin.random.Random
 
 class Firesweep(private val event: PlayerInteractEvent) : Ability(event.player, Bending.Fire) {
     private val cooldown = Duration.ofMillis(1500)
+
+    private val front = player.facing
+    private val left = front.perpendicular()
+    private val right = left.oppositeFace
 
     override fun preconditions() = with(event) {
         trigger { hasItem() }
@@ -30,6 +41,10 @@ class Firesweep(private val event: PlayerInteractEvent) : Ability(event.player, 
         PacketSender().send(player, Packet.Animation(player, ClientAnimation.SWING_MAIN_ARM))
 
         player.sendMessage("You used firesweep")
+        cone().forEach {
+            if (it.type == Material.AIR && Random.nextBoolean())
+                it.type = Material.FIRE
+        }
 
         player.setCooldown(item!!.type, cooldown.toTicks().toInt())
     }
@@ -38,4 +53,26 @@ class Firesweep(private val event: PlayerInteractEvent) : Ability(event.player, 
         @EventHandler fun sweep(e: PlayerInteractEvent) =
             Firesweep(e).execute()
     }
+
+    private fun cone(): List<Block> {
+        val f = player.location.block.getRelative(front)
+        val frontLine = (1..5).map {
+            f.getRelative(front, it)
+        }
+
+        val half = floor(frontLine.size / 2.0).roundToInt()
+        return frontLine.flatMapIndexed { index, block ->
+            line(block, -abs(index - half) + half + 1)
+        }.flatMap {
+            column(it)
+        }
+    }
+
+    private fun line(center: Block, length: Int) =
+        listOf(center) +
+            (1..length).map { center.getRelative(left, it) } +
+            (1..length).map { center.getRelative(right, it) }
+
+    private fun column(center: Block) =
+        listOf(center, center.getRelative(BlockFace.UP), center.getRelative(BlockFace.DOWN))
 }
